30c170dd57480f9aff3e43e10360626f07bc3651
Ruben01077 <102193579+Ruben01077@users.noreply.github.com>
2022-05-04 11:34:58 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let bookButtonLexus = document.getElementById("bookButtonLexus").addEventListener("click", function(){
+
+
+window.location.href = "https://turo.com/us/en/suv-rental/united-states/los-angeles-ca/lexus/rx/792460";
+
+
+})
+
+let bookButtonPrius = document.getElementById("bookButtonPrius").addEventListener("click", function() {
+
+
+window.location.href = "https://turo.com/us/en/car-rental/united-states/los-angeles-ca/toyota/prius/1448422";
+
+})
+
+
+let bookButtonPriusPrime1 = document.getElementById("bookButtonPriusPrime1").addEventListener("click", function(){
+
+
+window.location.href = "https://turo.com/us/en/car-rental/united-states/los-angeles-ca/toyota/prius-prime/898899";
+
+
+})
+
+let bookButtonPriusPrime2 = document.getElementById("bookButtonPriusPrime2").addEventListener("click", function(){
+
+
+    window.location.href = "https://turo.com/us/en/car-rental/united-states/los-angeles-ca/toyota/prius-prime/941731";
+    
+    
+    })
+
+let bookButtonMustang = document.getElementById("bookButtonMustang").addEventListener("click", function(){
+
+
+        window.location.href = "https://turo.com/us/en/car-rental/united-states/los-angeles-ca/ford/mustang/1468393";
+        
+        
+        })
+    
+
+
+
